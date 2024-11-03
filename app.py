78ad3956633c7bd0b47f1e70167340cccd3aaba5
R. E. Langley <langley@moonbeamclub.com>
2024-11-03 06:30:25 +0000

    Changed text

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Coin Data Visualization")
-st.write("Visualizing cryptocurrency data for Bitcoin, Ethereum, and Litecoin.")
+st.write("Visualizing cryptocurrency data")
